23363f6282c587b4f3044a6f01f35c5bfc9904f4
AnthonyMBA <anthonymarrablaza71099@gmail.com>
2025-03-22 21:36:50 +0800

    Update login_steps.py

--- login_auto_test/features/steps/login_steps.py
+++ login_auto_test/features/steps/login_steps.py
@@ -1,7 +1,6 @@
 from behave import given, when, then
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
