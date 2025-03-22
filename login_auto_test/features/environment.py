32c11840ad7f5f6e5fc1a77130ae94cbcd18277e
AnthonyMBA <anthonymarrablaza71099@gmail.com>
2025-03-22 21:49:11 +0800

    uploaded login_auto_test directory

---
+++ login_auto_test/features/environment.py
@@ -0,0 +1,11 @@
+import allure
+
+def before_scenario(context, scenario):
+    allure.dynamic.title(scenario.name)
+    
+
+def after_step(context, step):
+    if step.status == "failed":
+        allure.attach(context.driver.get_screenshot_as_png(),
+                      name="screenshot",
+                      attachment_type=allure.attachment_type.PNG)
